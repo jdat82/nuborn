f4e9263e2ff16244faa8ac528d8c10e076e7990c
jan4dev <jan4dev@gmail.com>
2013-06-22 13:18:49 +0200

    finished dependencies resolve algorithm

--- GruntUtils.js
+++ GruntUtils.js
@@ -110,10 +110,8 @@ var Util = {
         // Get all non sorted sources 
         var sources = grunt.file.expand(patterns)
         
-        if(debug) {
-            grunt.log.writeln()
-            grunt.log.writeln("SOURCES BEFORE RESOLVE PHASE: \n" + sources.join("\n") + "\n")
-        }
+        if(debug)
+            grunt.log.writeln("\nSOURCES BEFORE RESOLVE PHASE: \n" + sources.join("\n") + "\n")
 
         // Create the compiled object and array
         var providerMap = {}
@@ -179,43 +177,21 @@ var Util = {
             providerMap[provide] = node
         })
 
-        // Loop on allRequires to check if all requires has a corresponding provide
-        // for (var i = 0, length = allRequires.length; i < length; i++) {
-        //     var require = allRequires[i]
-        //     if (allProvides.indexOf(require) === -1) {
-        //         grunt.fail.warn("Missing provider : " + require + " is not provided !", 3)
-        //         return false
-        //     }
-        // }
-
         nodes.forEach(function(node){
             node.edges = []
             node.requires.forEach(function(require){
 
                 // first, let's see if every require has a provide
-                if (allProvides.indexOf(require) === -1) {
+                if (allProvides.indexOf(require) === -1)
                     grunt.fail.warn("Missing provider : " + require + " is not provided !", 3)
-                    grunt.log.writeln("\n\n SHOULD NOT BE PRINTED \n\n")
-                }
 
                 // second, let's change requires form from a string (provide) to an object node
                 node.edges.push(providerMap[require])
             })
         })
 
-        // def dep_resolve(node, resolved, unresolved):
-        // unresolved.append(node)
-        // for edge in node.edges:
-        //       if edge not in resolved:
-        //              if edge in unresolved:
-        //                     raise Exception('Circular reference detected: %s -> %s' % (node.name, edge.name))
-        //              dep_resolve(edge, resolved, unresolved)
-        // resolved.append(node)
-        // unresolved.remove(node)
-
         // final list of nodes in order
         var resolved=[]
-        grunt.log.writeln("\n NODES: \n" + Util.toJSON(nodes))
         
         // searching leafs nodes which are at the bottom of the tree
         var leafs = []
@@ -227,15 +203,13 @@ var Util = {
                 leafs.push(node)
         })
 
-        grunt.log.writeln("\n LEAFS NODES: \n" + leafs.length)
-        grunt.log.writeln("\n LEAFS NODES: \n" + Util.toJSON(leafs))
-        grunt.log.writeln("\n ALREADY RESOLVED NODES: \n" + Util.toJSON(nodes))
-
-        var resolve = function(node, resolved, unresolved) {
-            grunt.log.writeln("πππππππππππππππππππππππππππππππππππππππππππππππππππππππ")
+        // iterative function to climb the dependency tree
+        var resolve = function(node, resolved, unresolved) 
+        {
+            // keeping track of yet unresolved nodes
             unresolved.push(node)
-            grunt.log.writeln(node.source + ": " + Util.toJSON(node.requires))
-            node.edges.forEach(function(edge){
+
+            node.edges.forEach(function(edge) {
                 if(resolved.indexOf(edge) < 0)
                     // checking cyclic dependencies
                     if(unresolved.indexOf(edge) >= 0)
@@ -243,11 +217,16 @@ var Util = {
                     // digging again
                     resolve(edge, resolved, unresolved)
             })
-            resolved.push(node)
-            unresolved.splice(unresolved.indexOf(node), 1)
-        }
 
-        grunt.log.writeln("œœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœœ")
+            // adding if not present in list of resolved nodes
+            if(resolved.indexOf(node) < 0)
+                resolved.push(node)
+
+            // removing from list of unresolved nodes
+            var positionInUnresolved = unresolved.indexOf(node)
+            if(positionInUnresolved >= 0)
+                unresolved.splice(positionInUnresolved, 1)
+        }
 
         // climbing the resolve tree from the ground
         leafs.forEach(function(node){
@@ -255,74 +234,6 @@ var Util = {
             var unresolved=[]
             resolve(node, resolved, unresolved)
         })
-
-
-        // // create recursive method to list all requires of a source
-        // var recursiveListOfRequires = function(_object, _compiled, _listOfRequires) {
-        //     for (var i = 0, length = _object.requires.length; i < length; i++) {
-        //         var require = _object.requires[i]
-        //         if (_listOfRequires.indexOf(require) === -1) {
-        //             _listOfRequires.push(require)
-        //             recursiveListOfRequires(_compiled[require], _compiled, _listOfRequires)
-        //         }
-        //     }
-        // }
-
-        // // loop on compiled sources to get a complete list of requires including parents requires
-        // for (var i = 0, length = nodes.length; i < length; i++) {
-        //     // get the compile object
-        //     var compile = nodes[i]
-        //     // create an array for the full list of requires
-        //     var listOfRequires = []
-        //     // populate the list with the recursive method
-        //     recursiveListOfRequires(compile, compiled, listOfRequires)
-        //     // loop on provides of the object to find potential cyclic dependencies and prevent infinite loops
-        //     for (var j = 0, l = compile.provides.length; j < l; j++) {
-        //         // get the current provide
-        //         var provide = compile.provides[j]
-        //         // if the full list of requires contains the provide stop the task
-        //         if (listOfRequires.indexOf(provide) !== -1) {
-        //             grunt.fail.fatal("Error : " + compile.source + " raised cyclic dependencies !")
-        //             return false
-        //         }
-        //     }
-        //     // replace the requires array of the compile object with the full list
-        //     compile.requires = listOfRequires
-        // }
-
-        // if(debug)
-        //     grunt.log.writeln("\nDEPENDENCY TREE: \n" + Util.toJSON(nodes))
-
-        // // Sort the compiled array according to dependencies
-        // nodes.sort(function(a, b) 
-        // {
-        //     // if a requires a provide of b return 1
-        //     for (var i = 0, length = a.requires.length; i < length; i++) {
-        //         if (b.provides.indexOf(a.requires[i]) > -1) {
-        //             if(debug)
-        //                 grunt.log.writeln("a: " + a.source + " b: " + b.source + " res: 1")
-        //             return 1
-        //         }
-        //     }
-        //     // if b requires a provide of a return -1
-        //     for (var i = 0, length = b.requires.length; i < length; i++) {
-        //         if (a.provides.indexOf(b.requires[i]) > -1) {
-        //             if(debug)
-        //                 grunt.log.writeln("a: " + a.source + " b: " + b.source + " res: -1")
-        //             return -1
-        //         }
-        //     }
-
-        //     if(debug)
-        //         grunt.log.writeln("a: " + a.source + " b: " + b.source + " res: 0")
-
-        //     // else return 0
-        //     return 0
-
-        // })
-
-        if(debug)
-            grunt.log.writeln("\nDEPENDENCY TREE AFTER SORT: \n" + Util.toJSON(nodes))
 
         var results = []
 
