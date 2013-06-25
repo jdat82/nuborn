089e3d85afe39b5b71e4350a93bd9d637ecaffde
jan4dev <jan4dev@gmail.com>
2013-06-25 13:34:44 +0200

    Commented error interceptions as it does not work correctly and forbid correct debugging.

--- src/app/app.js
+++ src/app/app.js
@@ -8,9 +8,9 @@
  */
 var app = {}
 
-window.onerror = function(message, url, line){
-	log.e(message);
-};
+// window.onerror = function(message, url, line){
+// 	log.e(message);
+// };
 
 // Create Shortcuts for singletons
 var utils = nu.Utils;
