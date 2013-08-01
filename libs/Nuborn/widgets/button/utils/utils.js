1756e532f1edc6cbb9e99ad7c642bc58c37bb64c
jan4dev <jan4dev@gmail.com>
2013-08-01 13:31:55 +0200

    Javascript documentation

--- libs/Nuborn/widgets/button/utils/utils.js
+++ libs/Nuborn/widgets/button/utils/utils.js
@@ -12,28 +12,44 @@
 	 */
 	nu.widgets.button.Utils = {
 
+		/**
+		 * Handle active state on button the portable way.
+		 * i.e compatible with Android 2.3 and browsers without :active pseudo class support.
+		 */
 		onvmousedown: function() {
 			var button = $(this)
-			// making the back button active
+			// making the button active
 			button.addClass("pressed")
 			// when touch end, go to normal state
 			button.one("vmouseup vmousemove", function() {
-				// making the back button normal
+				// making the button normal
 				button.removeClass("pressed")
 			})
 		},
 
+		/**
+		 * Enable button's press mode.
+		 *
+		 * {@link #onvmousedown See onvmousedown}
+		 */
 		enableUniversalPressMode: function(button) {
 			if (!button) return;
+			// getting a jQuery reference
 			if (!button.context) button = $(button)
-
+			// enabling press mode	
 			button.on("vmousedown", this.onvmousedown)
 		},
 
+		/**
+		 * Disable button's press mode.
+		 *
+		 * {@link #onvmousedown See onvmousedown}
+		 */
 		disableUniversalPressMode: function(button) {
 			if (!button) return;
+			// getting a jQuery reference
 			if (!button.context) button = $(button)
-
+			// disabling press mode
 			button.off("vmousedown", this.onvmousedown)
 		}
 
