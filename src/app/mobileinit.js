b675f8c6cae136dda3c3569e105469706b355b6e
jan4dev <jan4dev@gmail.com>
2013-08-21 00:20:08 +0200

    Moved defaultPageTransition initialisation in mobileinit and replaced android test with gradeA JQM test

--- src/app/mobileinit.js
+++ src/app/mobileinit.js
@@ -1,8 +1,22 @@
 // Configuration of jQuery Mobile before it loads
-$(document).on("mobileinit", function () {
+$(document).on("mobileinit", function ()
+{
 	// do not auto initialize page because of splashscreen
 	$.mobile.autoInitializePage = false;
+
 	// $.mobile.touchOverflowEnabled = true; // Deprecated since 1.1
-	$.mobile.phonegapNavigationEnabled = true;
+
+	// phonegap friendly ?
+	if (window.cordova)
+		$.mobile.phonegapNavigationEnabled = true;
+
+	// TODO je serais partant pour désactiver les transitions dès qu'on a un peu scrollé
+	// la valeur par défaut me semble bien trop importante
 	// $.mobile.maxScrollForTransition = 1;
+
+	// Don't use fancy transitions on poor capable devices
+    if ($.mobile.gradeA())
+        $.mobile.defaultPageTransition = "slide";
+    else
+		$.mobile.defaultPageTransition = "none";
 });
