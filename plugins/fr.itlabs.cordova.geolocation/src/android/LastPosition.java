5ee70c0220045db7e42070c9e4ced35fac3bd88a
jan4dev <jan4dev@gmail.com>
2014-03-22 13:02:16 +0100

    New traces to better understand from whom the position is acquired.

--- plugins/fr.itlabs.cordova.geolocation/src/android/LastPosition.java
+++ plugins/fr.itlabs.cordova.geolocation/src/android/LastPosition.java
@@ -11,11 +11,14 @@ import org.json.JSONObject;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
+import android.util.Log;
 
 
 
 public class LastPosition extends CordovaPlugin {
-	
+
+    private static final String TAG = "LastPosition";
+
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getOSLanguage")){
@@ -28,8 +31,8 @@ public class LastPosition extends CordovaPlugin {
         }
         return false;
     }
-	
-	
+
+
 	private void getOSLanguage(CallbackContext callbackContext){
 		try{
     		JSONObject data = new JSONObject();
@@ -41,47 +44,54 @@ public class LastPosition extends CordovaPlugin {
 			callbackContext.error("LANGUAGE EXCEPTION");
 		}
 	}
-	
+
     private void get(CallbackContext callbackContext) {
         try{
         	LocationManager locationManager = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
-        	
+
         	// Detection de la position en live, processus pouvant prendre a utiliser si le LastKnowLocation
         	//LocationListener mlocListener = new AldoLocationListener(callbackContext, locationManager);
         	//locationManager.requestLocationUpdates( LocationManager.NETWORK_PROVIDER, 0, 0, mlocListener);
-        	
-        	
+
+
         	// On recupere la derniere position reseau
         	Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         	if(loc != null){
         		JSONObject position = new JSONObject();
         		position.put("provider", "GPS");
-            	
+
         		JSONObject coords = new JSONObject();
             	coords.put("latitude", loc.getLatitude());
             	coords.put("longitude", loc.getLongitude());
             	position.put("coords", coords);
-            	
+
+                Log.i(TAG, "Found last position from the GPS provider: " + loc.getLatitude() + ", " + loc.getLongitude());
+
                 callbackContext.success(position);
                 return;
-        	} 
+        	}
         	// Si il n'y en a pas, on recupere la derniere position GPS
         	loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         	if(loc != null){
         		JSONObject position = new JSONObject();
             	position.put("provider", "NETWORK");
-            	
+
         		JSONObject coords = new JSONObject();
             	coords.put("latitude", loc.getLatitude());
             	coords.put("longitude", loc.getLongitude());
             	position.put("coords", coords);
-            	
+
+                Log.i(TAG, "Found last position from the NETWORK provider: " + loc.getLatitude() + ", " + loc.getLongitude());
+
                 callbackContext.success(position);
         		return;
         	}
+
+            Log.i(TAG, "Last position unavailable");
         	callbackContext.error("CACHED_POSITION_UNAVAILABLE");
-        	
+
         } catch (JSONException e){
+            Log.e(TAG, "Error while searching last position", e);
         	callbackContext.error("GEOLOC EXCEPTION");
         }
     }
